9ecbb567224968ea92bb4c3431bed6eeb5f53920
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-12-15 15:25:51 -0500

    Update Makefile to lint and test docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import littlemcmc
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # -- Project information -----------------------------------------------------
