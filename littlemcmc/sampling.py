e72bb0f21132f12f260d40d97fc47959a9536efc
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-12-26 11:42:40 +0800

    Initial dump of init_nuts from PyMC3

--- littlemcmc/sampling.py
+++ littlemcmc/sampling.py
@@ -127,3 +127,115 @@ def sample(
     stats = [chain_stats for (_, chain_stats) in results]  # FIXME reshape stats
 
     return trace, stats
+
+
+def init_nuts(
+    init="auto",
+    chains=1,
+    n_init=500000,
+    model=None,
+    random_seed=None,
+    progressbar=True,
+    **kwargs,
+):
+    """Set up the mass matrix initialization for NUTS.
+    NUTS convergence and sampling speed is extremely dependent on the
+    choice of mass/scaling matrix. This function implements different
+    methods for choosing or adapting the mass matrix.
+    Parameters
+    ----------
+    init: str
+        Initialization method to use.
+        * auto : Choose a default initialization method automatically.
+          Currently, this is `'jitter+adapt_diag'`, but this can change in the future. If you
+          depend on the exact behaviour, choose an initialization method explicitly.
+        * adapt_diag : Start with a identity mass matrix and then adapt a diagonal based on the
+          variance of the tuning samples. All chains use the test value (usually the prior mean)
+          as starting point.
+        * jitter+adapt_diag : Same as ``adapt_diag``, but use uniform jitter in [-1, 1] as starting
+          point in each chain.
+        * advi+adapt_diag : Run ADVI and then adapt the resulting diagonal mass matrix based on the
+          sample variance of the tuning samples.
+        * advi+adapt_diag_grad : Run ADVI and then adapt the resulting diagonal mass matrix based
+          on the variance of the gradients during tuning. This is **experimental** and might be
+          removed in a future release.
+        * advi : Run ADVI to estimate posterior mean and diagonal mass matrix.
+        * advi_map: Initialize ADVI with MAP and use MAP as starting point.
+        * map : Use the MAP as starting point. This is discouraged.
+        * nuts : Run NUTS and estimate posterior mean and mass matrix from
+          the trace.
+    chains: int
+        Number of jobs to start.
+    n_init: int
+        Number of iterations of initializer
+        If 'ADVI', number of iterations, if 'nuts', number of draws.
+    model: Model (optional if in ``with`` context)
+    progressbar: bool
+        Whether or not to display a progressbar for advi sampling.
+    **kwargs: keyword arguments
+        Extra keyword arguments are forwarded to pymc3.NUTS.
+    Returns
+    -------
+    start: pymc3.model.Point
+        Starting point for sampler
+    nuts_sampler: pymc3.step_methods.NUTS
+        Instantiated and initialized NUTS sampler object
+    """
+    model = modelcontext(model)
+
+    vars = kwargs.get("vars", model.vars)
+    if set(vars) != set(model.vars):
+        raise ValueError("Must use init_nuts on all variables of a model.")
+    if not all_continuous(vars):
+        raise ValueError(
+            "init_nuts can only be used for models with only " "continuous variables."
+        )
+
+    if not isinstance(init, str):
+        raise TypeError("init must be a string.")
+
+    if init is not None:
+        init = init.lower()
+
+    if init == "auto":
+        init = "jitter+adapt_diag"
+
+    _log.info("Initializing NUTS using {}...".format(init))
+
+    if random_seed is not None:
+        random_seed = int(np.atleast_1d(random_seed)[0])
+        np.random.seed(random_seed)
+
+    cb = [
+        pm.callbacks.CheckParametersConvergence(tolerance=1e-2, diff="absolute"),
+        pm.callbacks.CheckParametersConvergence(tolerance=1e-2, diff="relative"),
+    ]
+
+    if init == "adapt_diag":
+        start = [model.test_point] * chains
+        mean = np.mean([model.dict_to_array(vals) for vals in start], axis=0)
+        var = np.ones_like(mean)
+        potential = quadpotential.QuadPotentialDiagAdapt(model.ndim, mean, var, 10)
+    elif init == "jitter+adapt_diag":
+        start = []
+        for _ in range(chains):
+            mean = {var: val.copy() for var, val in model.test_point.items()}
+            for val in mean.values():
+                val[...] += 2 * np.random.rand(*val.shape) - 1
+            start.append(mean)
+        mean = np.mean([model.dict_to_array(vals) for vals in start], axis=0)
+        var = np.ones_like(mean)
+        potential = quadpotential.QuadPotentialDiagAdapt(model.ndim, mean, var, 10)
+    elif init == "nuts":
+        init_trace = pm.sample(
+            draws=n_init, step=pm.NUTS(), tune=n_init // 2, random_seed=random_seed
+        )
+        cov = np.atleast_1d(pm.trace_cov(init_trace))
+        start = list(np.random.choice(init_trace, chains))
+        potential = quadpotential.QuadPotentialFull(cov)
+    else:
+        raise ValueError("Unknown initializer: {}.".format(init))
+
+    step = pm.NUTS(potential=potential, model=model, **kwargs)
+
+    return start, step
