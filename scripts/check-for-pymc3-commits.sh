dda9aae6020d12617bdf75c8c5ea4fccdac3ae7e
George Ho <19851673+eigenfoo@users.noreply.github.com>
2020-04-25 11:32:58 -0400

    BLD: add checks for PyMC3 commits

---
+++ scripts/check-for-pymc3-commits.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Checks if there have been any commits to pymc3/step_methods/ since yesterday.
+
+set -e
+
+git clone https://github.com/pymc-devs/pymc3.git
+cd pymc3/
+num_commits=$(git log --since="1 day ago" pymc3/step_methods/ | grep "commit" | wc -l)
+
+if [ "$num_commits" -eq "0" ]; then
+    echo "No commits since yesterday. Passing."
+    exit 0
+else
+    echo "$num_commits commits since yesterday. Failing."
+    echo ""
+    git log --since="1 day ago" pymc3/step_methods/ | grep "commit" | sed "s/commit /https:\/\/github.com\/pymc-devs\/pymc3\/commit\//"
+    exit 1
+fi
