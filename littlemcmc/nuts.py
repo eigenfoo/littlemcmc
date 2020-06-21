MAINT: fix numerical issue with acceptance ratio (#73)
* BUG: fix numerical issue with the NUTS acceptance ratio

* MAINT: move functions to a math module

* MAINT: actually add math module

* BUG: remove yml parameter

* STY: black

* STY: pylint and mypy

littlemcmc/nuts.py
@@ -20,19 +20,13 @@ from collections import namedtuple
 from typing import Callable, Tuple, List, Optional
 
 import numpy as np
-import numpy.random as nr
 
+from .math import logbern, logdiffexp_numpy
 from .base_hmc import BaseHMC, HMCStepData, DivergenceInfo
 from .integration import IntegrationError
 from .report import SamplerWarning, WarningType
 
 __all__ = ["NUTS"]
-
-
-def logbern(log_p: float) -> bool:
-    if np.isnan(log_p):
-        raise FloatingPointError("log_p can't be nan.")
-    return np.log(nr.uniform()) < log_p
 
 
 class NUTS(BaseHMC):
@@ -246,10 +240,12 @@ class NUTS(BaseHMC):
 
 
 # A proposal for the next position
-Proposal = namedtuple("Proposal", "q, q_grad, energy, p_accept, logp")
+Proposal = namedtuple("Proposal", "q, q_grad, energy, log_p_accept_weighted, logp")
 
 # A subtree of the binary tree built by nuts.
-Subtree = namedtuple("Subtree", "left, right, p_sum, proposal, log_size, accept_sum, n_proposals")
+Subtree = namedtuple(
+    "Subtree", "left, right, p_sum, proposal, log_size, log_weighted_accept_sum, n_proposals"
+)
 
 
 class _Tree(object):
@@ -279,7 +275,8 @@ class _Tree(object):
         self.proposal = Proposal(start.q, start.q_grad, start.energy, 1.0, start.model_logp)
         self.depth = 0
         self.log_size = 0
-        self.accept_sum = 0
+        self.log_weighted_accept_sum = -np.inf
+        self.mean_tree_accept = 0.0
         self.n_proposals = 0
         self.p_sum = start.p.copy()
         self.max_energy_change = 0
@@ -300,15 +297,22 @@ class _Tree(object):
             tree, diverging, turning = self._build_subtree(
                 self.right, self.depth, np.asarray(self.step_size)
             )
+            leftmost_begin, leftmost_end = self.left, self.right
+            rightmost_begin, rightmost_end = tree.left, tree.right
+            leftmost_p_sum = self.p_sum
+            rightmost_p_sum = tree.p_sum
             self.right = tree.right
         else:
             tree, diverging, turning = self._build_subtree(
                 self.left, self.depth, np.asarray(-self.step_size)
             )
+            leftmost_begin, leftmost_end = tree.right, tree.left
+            rightmost_begin, rightmost_end = self.left, self.right
+            leftmost_p_sum = tree.p_sum
+            rightmost_p_sum = self.p_sum
             self.left = tree.right
 
         self.depth += 1
-        self.accept_sum += tree.accept_sum
         self.n_proposals += tree.n_proposals
 
         if diverging or turning:
@@ -319,11 +323,21 @@ class _Tree(object):
             self.proposal = tree.proposal
 
         self.log_size = np.logaddexp(self.log_size, tree.log_size)
+        self.log_weighted_accept_sum = np.logaddexp(
+            self.log_weighted_accept_sum, tree.log_weighted_accept_sum
+        )
         self.p_sum[:] += tree.p_sum
 
-        left, right = self.left, self.right
-        p_sum = self.p_sum
-        turning = (p_sum.dot(left.v) <= 0) or (p_sum.dot(right.v) <= 0)
+        # Additional turning check only when tree depth > 0 to avoid redundant work
+        if self.depth > 0:
+            left, right = self.left, self.right
+            p_sum = self.p_sum
+            turning = (p_sum.dot(left.v) <= 0) or (p_sum.dot(right.v) <= 0)
+            p_sum1 = leftmost_p_sum + rightmost_begin.p
+            turning1 = (p_sum1.dot(leftmost_begin.v) <= 0) or (p_sum1.dot(rightmost_begin.v) <= 0)
+            p_sum2 = leftmost_end.p + rightmost_p_sum
+            turning2 = (p_sum2.dot(leftmost_end.v) <= 0) or (p_sum2.dot(rightmost_end.v) <= 0)
+            turning = turning | turning1 | turning2
 
         return diverging, turning
 
@@ -342,10 +356,16 @@ class _Tree(object):
             if np.abs(energy_change) > np.abs(self.max_energy_change):
                 self.max_energy_change = energy_change
             if np.abs(energy_change) < self.Emax:
-                p_accept = min(1, np.exp(-energy_change))
+                # Acceptance statistic
+                # e^{H(q_0, p_0) - H(q_n, p_n)} max(1, e^{H(q_0, p_0) - H(q_n, p_n)})
+                # Saturated Metropolis accept probability with Boltzmann weight
+                # if h - H0 < 0
+                log_p_accept_weighted = -energy_change + min(0.0, -energy_change)
                 log_size = -energy_change
-                proposal = Proposal(right.q, right.q_grad, right.energy, p_accept, right.model_logp)
-                tree = Subtree(right, right, right.p, proposal, log_size, p_accept, 1)
+                proposal = Proposal(
+                    right.q, right.q_grad, right.energy, log_p_accept_weighted, right.model_logp
+                )
+                tree = Subtree(right, right, right.p, proposal, log_size, log_p_accept_weighted, 1)
                 return tree, None, False
             else:
                 error_msg = "Energy change in leapfrog step is too large: %s." % energy_change
@@ -369,8 +389,18 @@ class _Tree(object):
         if not (diverging or turning):
             p_sum = tree1.p_sum + tree2.p_sum
             turning = (p_sum.dot(left.v) <= 0) or (p_sum.dot(right.v) <= 0)
+            # Additional U turn check only when depth > 1 to avoid redundant work.
+            if depth - 1 > 0:
+                p_sum1 = tree1.p_sum + tree2.left.p
+                turning1 = (p_sum1.dot(tree1.left.v) <= 0) or (p_sum1.dot(tree2.left.v) <= 0)
+                p_sum2 = tree1.right.p + tree2.p_sum
+                turning2 = (p_sum2.dot(tree1.right.v) <= 0) or (p_sum2.dot(tree2.right.v) <= 0)
+                turning = turning | turning1 | turning2
 
             log_size = np.logaddexp(tree1.log_size, tree2.log_size)
+            log_weighted_accept_sum = np.logaddexp(
+                tree1.log_weighted_accept_sum, tree2.log_weighted_accept_sum
+            )
             if logbern(tree2.log_size - log_size):
                 proposal = tree2.proposal
             else:
@@ -378,18 +408,25 @@ class _Tree(object):
         else:
             p_sum = tree1.p_sum
             log_size = tree1.log_size
+            log_weighted_accept_sum = tree1.log_weighted_accept_sum
             proposal = tree1.proposal
 
-        accept_sum = tree1.accept_sum + tree2.accept_sum
         n_proposals = tree1.n_proposals + tree2.n_proposals
 
-        tree = Subtree(left, right, p_sum, proposal, log_size, accept_sum, n_proposals)
+        tree = Subtree(left, right, p_sum, proposal, log_size, log_weighted_accept_sum, n_proposals)
         return tree, diverging, turning
 
     def stats(self):
+        # Update accept stat if any subtrees were accepted
+        if self.log_size > 0:
+            # Remove contribution from initial state which is always a perfect
+            # accept
+            log_sum_weight = logdiffexp_numpy(self.log_size, 0.0)
+            self.mean_tree_accept = np.exp(self.log_weighted_accept_sum - log_sum_weight)
+
         return {
             "depth": self.depth,
-            "mean_tree_accept": self.accept_sum / self.n_proposals,
+            "mean_tree_accept": self.mean_tree_accept,
             "energy_error": self.proposal.energy - self.start.energy,
             "energy": self.proposal.energy,
             "tree_size": self.n_proposals,