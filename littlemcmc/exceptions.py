2f579ac592fe9fa9ddb726f296f0c526e418b30c
George Ho <19851673+eigenfoo@users.noreply.github.com>
2020-08-04 20:59:37 -0400

    MAINT: remove unused exceptions

--- littlemcmc/exceptions.py
+++ littlemcmc/exceptions.py
@@ -14,56 +14,8 @@
 
 __all__ = [
     "SamplingError",
-    "IncorrectArgumentsError",
-    "TraceDirectoryError",
-    "ImputationWarning",
-    "ShapeError",
 ]
 
 
 class SamplingError(RuntimeError):
     pass
-
-
-class IncorrectArgumentsError(ValueError):
-    pass
-
-
-class TraceDirectoryError(ValueError):
-    """Error from trying to load a trace from an incorrectly-structured directory,"""
-
-    pass
-
-
-class ImputationWarning(UserWarning):
-    """Warning that there are missing values that will be imputed."""
-
-    pass
-
-
-class ShapeError(Exception):
-    """Error that the shape of a variable is incorrect."""
-
-    def __init__(self, message, actual=None, expected=None):
-        if actual is not None and expected is not None:
-            super().__init__("{} (actual {} != expected {})".format(message, actual, expected))
-        elif actual is not None and expected is None:
-            super().__init__("{} (actual {})".format(message, actual))
-        elif actual is None and expected is not None:
-            super().__init__("{} (expected {})".format(message, expected))
-        else:
-            super().__init__(message)
-
-
-class DtypeError(TypeError):
-    """Error that the dtype of a variable is incorrect."""
-
-    def __init__(self, message, actual=None, expected=None):
-        if actual is not None and expected is not None:
-            super().__init__("{} (actual {} != expected {})".format(message, actual, expected))
-        elif actual is not None and expected is None:
-            super().__init__("{} (actual {})".format(message, actual))
-        elif actual is None and expected is not None:
-            super().__init__("{} (expected {})".format(message, expected))
-        else:
-            super().__init__(message)
